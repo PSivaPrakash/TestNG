33dd67246a789433055f1cb1386c4a94b66699a4
PSivaPrakash <56464550+PSivaPrakash@users.noreply.github.com>
2020-02-17 17:37:04 +0530

    First TestNG Commit

---
+++ TestNG/src/com/offline/TestNG.java
@@ -0,0 +1,27 @@
+package com.offline;
+
+import org.testng.annotations.Test;
+
+public class TestNG 
+{	
+	
+	
+	
+/*	@Test(priority=1,description ="This test case will launch the browser")
+	public void LaunchBrowser()
+	{
+		System.out.println("Browser Launched");
+	}
+	
+	@Test(priority=2,description ="This test case will land on home page")
+	public void LandOnHomePage()
+	{
+		System.out.println("Landed on Home Page");
+	}
+	
+	@Test(priority=3,description ="This test case will close the browser")
+	public void CloseBrowser()
+	{
+		System.out.println("Browser is Closed");
+	} */
+}
